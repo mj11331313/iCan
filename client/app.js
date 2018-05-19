126a16b74a90556d77c35a51ec292b98d4020f56
skygodbas <cynthia_hfe@163.com>
2018-05-19 08:59:20 +0800

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,9 +1,34 @@
-//app.js
-var qcloud = require('./vendor/wafer2-client-sdk/index')
-var config = require('./config')
-
+const appid = 'APPID';
+const secret = 'SECRET';
 App({
-    onLaunch: function () {
-        qcloud.setLoginUrl(config.service.loginUrl)
-    }
+  onShow:function(){
+    // 获取code
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          console.log('获取code成功');
+          // 发起请求获取openid并保存
+          wx.request({
+            url: `https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${res.code}&grant_type=authorization_code`,
+            success:res => {
+              console.log('获取openid成功');
+              wx.setStorageSync('openid',res.data.openid);
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg);
+        }
+      }
+    });
+    // 获取access_token并保存
+    wx.request({
+      url:`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}`,
+      method:'GET',
+      success:res => {
+        console.log('获取access_token成功');
+        console.log(res);
+        wx.setStorageSync('access_token', res.data.access_token);
+      }
+    })
+  }
 })
